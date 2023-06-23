4bb743f1ce11f0a94d3675ca988734527fbb54b9
Josh Joby <85182505+JoshJoby@users.noreply.github.com>
2023-06-23 13:22:25 +0530

    added welcome.html icons

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function(event) {
     console.log('Activated app.js', event);
 });
+
+const express = require('express');
+const app = express();
