6b74ca6d4dbc3109e3e9683552995af60506a46a
Josh Joby <85182505+JoshJoby@users.noreply.github.com>
2023-06-23 19:31:40 +0530

    added index-user.html

--- app.js
+++ app.js
@@ -5,6 +5,3 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function(event) {
     console.log('Activated app.js', event);
 });
-
-const express = require('express');
-const app = express();
