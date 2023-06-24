49d9dbf4ecef19aa9ea7d1269adabfd68820a032
mansoor <mansoorprojects75@gmail.com>
2023-06-24 11:35:26 +0530

    added toast

--- backdata/index.js
+++ backdata/index.js
@@ -1,32 +1,38 @@
-
-
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
-const axios = require("axios");
-const nodemailer = require("nodemailer");
-app.use(bodyParser.json());
-// app.use(express.static('public'))
-
-const port = process.env.PORT ||9000
+const cors = require('cors');
+const path = require("path");
+const nodemailer=require('nodemailer')
+let formidable = require('formidable');
+let fs = require('fs');
+const bodyParser = require("body-parser");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const mysql = require("mysql");
 
-// parse application/json
+
 app.use(bodyParser.json());
+app.use(express.static('public'))
+
+
+
+
+
+app.use(cors())
+
+const port = process.env.PORT ||9000
 
 //Create Database Connection
 const conn = mysql.createConnection({
-	// host: "localhost",
-	// user: "root",
-	// password: "",
-	// database: "api",
+	host: "localhost",
+	user: "root",
+	password: "",
+	database: "pranicure",
 
-	host: "bvccsm8jfuuoms3dyzxc-mysql.services.clever-cloud.com",
-	user: "ufc6wtg8pxguvx91",
-	password: "DFSXXuSMABl6C9ONUzj2",
-	database: "bvccsm8jfuuoms3dyzxc",
+	// host: "bvccsm8jfuuoms3dyzxc-mysql.services.clever-cloud.com",
+	// user: "ufc6wtg8pxguvx91",
+	// password: "DFSXXuSMABl6C9ONUzj2",
+	// database: "bvccsm8jfuuoms3dyzxc",
 });
 
 // connect to database
@@ -36,10 +42,46 @@ conn.connect((err) => {
 });
 
 
+app.post("/login", (req, res) => {
+	let sql = `SELECT * FROM std_user where email="${req.body.user}" `;
+	let query = conn.query(sql, (err, result) => {
+		if (result.length!=0) {
+		if (req.body.Pass==result[0].password) {
+			res.send(JSON.stringify({ status: 200, error: null, response: result }));
+		}
+		else{
+			res.send(JSON.stringify({ status: 500, error: null, response: result }));
+			
+		}
+	}
+	else{
+		res.send(JSON.stringify({ status: 700, error: null, response: result }));
 
+	}
+	})
+
+	})
 app.post("/signup", (req, res) => {
-	let data = { name: req.body.name, phone: req.body.phone , roll:req.body.roll};
-	let sql = "INSERT INTO testapi SET ?";
+	const date = new Date();
+
+// Format the date as "dd/mm/yy"
+const formattedDate = date.toLocaleDateString('en-US', {
+  day: '2-digit',
+  month: '2-digit',
+  year: '2-digit'
+});
+
+// Get the day of the week
+const dayOfWeek = date.toLocaleString('en-US', {
+  weekday: 'long'
+});
+
+// Combine the formatted date and day of the week
+const timeString = `${formattedDate} ${dayOfWeek}`;
+	let data = { user_id: req.body. Userid,username: req.body.Usr, user_type: req.body.Mode , 
+		password:req.body.passwd, email:req.body.Mail, phone_number	:req.body.Phone, address:req.body.address,profile_picture:req.body.img, created_at:timeString
+	};
+	let sql = "INSERT INTO std_user SET ?";
 	let query = conn.query(sql, data, (err, result) => {
 		if (err) throw err;
 		res.send(JSON.stringify({ status: 200, error: null, response: "New Record is Added successfully" }));
@@ -48,15 +90,116 @@ app.post("/signup", (req, res) => {
 
 
 
-app.post("/signupmail", (req, res) => {
-	let data = { name: req.body.Usr, email: req.body.mail , otp:req.body.otp};
-	let sql = "INSERT INTO testapi SET ?";
-	let query = conn.query(sql, data, (err, result) => {
-		if (err) throw err;
-		res.send(JSON.stringify({ status: 200, error: null, response: "New Record is Added successfully" }));
-	});
+app.post("/signupemail",async (req, res) => {
+
+	let data = { email_id: req.body.Mail , otp:req.body.Otp,phone:req.body.Phone};
+
+	let sqld = `DELETE FROM otp where email_id="${req.body.Mail}" `;
+		let queryd = conn.query(sqld, (err, result) => {
+			if (err) throw err;
+
+
+
+
+	
+	
+		let sql = "INSERT INTO otp SET ?";
+		let query = conn.query(sql, data, (err, result) => {
+			if (err) res.send(JSON.stringify({ status: 500, error: null}));
+			else{
+
+				let transporter = nodemailer.createTransport({
+					// host: "smtp.ethereal.email",
+					// port: 587,
+					service: "gmail", // true for 465, false for other ports
+					auth: {
+					  user: "webdearsproject@gmail.com", // generated ethereal user
+					  pass: "iefrtrdbsudvpsyx", // generated ethereal password
+					},
+				  });
+				
+				  var mailoptions = {
+					from: "webdearsproject@gmail.com",
+					to: req.body.Mail,
+					// subject:'Personal Secret Key From SH....Chat',
+					subject: "OTP from ParniCure",
+					html: `<h4>Dear ${req.body.Usr},</h4>
+					<h5>Here is your OTP FOR Creating Account</h5>
+					<h3>${req.body.Otp}</h3>`,
+					// html:`<p>Don't share This qr <b> </b> </p><p>
+					// <img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}" alt=""></p>`
+				  };
+				
+				  transporter.sendMail(mailoptions, async function (err, info) {
+					if (err) {
+						res.send(JSON.stringify({ status: 500, error: null, response: "New Record is Added successfully" }));
+					 
+				  }
+				  else{
+				  
+					res.send(JSON.stringify({ status: 200, error: null, response: "New Record is Added successfully" }));
+				
+				  }
+				  
+				  });
+			}
+		})
+	})
+	
+	
 });
 
+
+
+app.get("/validateotp", (req, res) => {
+	console.log(req.query.mail)
+	console.log(req.query.mail)
+	console.log(req.query.otpval)
+	// console.log(req.query.otpval)
+
+	let sql = `SELECT * FROM otp where email_Id="${req.query.mail}" `;
+	let query = conn.query(sql, (err, result) => {
+		console.log(result[0].otp)
+		if (req.query.otpval!=result[0].otp) {
+			res.send(JSON.stringify({ status: 500, error: null, response: result }));
+		}
+
+		else if(req.query.otpval==result[0].otp){
+		
+
+					res.send(JSON.stringify({ status: 200, error: null, response: result }));
+
+		}
+		else{
+			res.send(JSON.stringify({ status: 700, error: null, response: result }));
+			
+		}
+		
+	});
+});
+app.post(
+	"/upload",
+	// upload.single("picfile" /* name attribute of <file> element in your form */),
+	(req, res) => {
+		let form = new formidable.IncomingForm();
+		form.parse(req, function (error, fields, file) {
+			let filepath = file.picfile.filepath;
+			let newpath = "C:/PraniCare/userapp/public/profileimages/";
+			
+			newpath += file.picfile.originalFilename;
+			console.log(filepath)
+		
+			//Copy the uploaded file to a custom folder
+			fs.rename(filepath, newpath,  err => {
+				if (err) res.send(JSON.stringify({ status: 500, error: 1, response:newpath}));
+		
+				res.send(JSON.stringify({ status: 200, error: null, response:newpath}))
+			  });
+			
+			  
+		  });
+		}
+  );
 
 
 async function mail1(to, sub, body,res) {
--- userapp/public/login.js
+++ userapp/public/login.js
@@ -1,4 +1,69 @@
 let query =window.location.search
 let url =new URLSearchParams(query)
 let val=url.get('mode')
-alert(val)
+$("#create").on("click",function(){
+    window.location.href=`signup.html?mode=${val}`
+})
+
+
+$("#login").on("click",function(){
+
+    var name=$("#name").val();
+    var pass=$("#pass").val();
+    if(val==null){
+        alert("select the type of user");
+        setTimeout(function(){
+        window.location.href='welcome.html'
+
+
+    
+    },1000)
+    return;
+    }
+    if(name==''|| name==' '){
+       alert("Invalid Mail")
+        // toastr.warning("Please Enter a valid Mail", "Mail  Error", { positionClass: "toast-top-right", timeOut: 5e3, closeButton: !0, debug: !1, newestOnTop: !0, progressBar: !0, preventDuplicates: !0, onclick: null, showDuration: "300", hideDuration: "1000", extendedTimeOut: "1000", showEasing: "swing", hideEasing: "linear", showMethod: "fadeIn", hideMethod: "fadeOut", tapToDismiss: !1 }) 
+        return;
+        
+           
+        }
+        if(pass=='' ){
+        
+        alert("pass");
+        // toastr.warning("Please Enter valid Paaword having more that 5 characters", "Password  Error", { positionClass: "toast-top-right", timeOut: 5e3, closeButton: !0, debug: !1, newestOnTop: !0, progressBar: !0, preventDuplicates: !0, onclick: null, showDuration: "300", hideDuration: "1000", extendedTimeOut: "1000", showEasing: "swing", hideEasing: "linear", showMethod: "fadeIn", hideMethod: "fadeOut", tapToDismiss: !1 }) 
+        return;
+        
+        
+        }
+        $.ajax({
+            url:"http://localhost:9000/login",
+            type:"post",
+            dataType: "json",
+            data: { user:name,Pass:pass
+            },
+            success: function(res){
+                console.log(res.response)
+                if(res.status==200){
+                    swal("Logged In Successfully")
+                    setTimeout(function(){
+                        window.location.href='index.html'
+                        localStorage.setItem('prani',`${res.response[0].user_id}-${val}`)
+
+                    
+                    },2000)
+                }
+                else if(res.status==500){
+                    swal("Failed", "No user exist with given Credentials ", "error");
+
+                }
+                else{
+                    alert("No user exist")
+                }
+
+            }
+        })
+
+
+
+
+})
